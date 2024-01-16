9315da7ec5d2641b89be8c82511da42753d031d2
leluong90 <luong.lv2804@gmail.com>
2024-01-16 22:03:38 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal'
+import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'FLyin',
+  description: 'Booking App',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClientOnly>
+          <ToasterProvider/>
+          <RentModal/>
+          <SearchModal/>
+          <LoginModal/>
+          <RegisterModal/>
+          <Navbar currentUser = {currentUser}/>
+        </ClientOnly>
+        <div className='pb-20 pt-28'>
+        {children}
+
+        </div>
+        
+        
+        </body>
+    </html>
+  )
+}
